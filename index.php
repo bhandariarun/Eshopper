519dd387b9537e5b0fa649709ebcdd06daf5c128
arun <abbhan053@gmail.com>
2022-12-10 14:37:11 +0545

    arun

--- index.php
+++ index.php
@@ -143,7 +143,7 @@ session_start();
                         <?php
 						$result=$conn->query("SELECT * FROM Products");
 						while($row = $result->fetch_assoc()) {
-							echo "<div class='col-sm-4'>";
+							echo "<div class='col-sm-3'>";
 							echo "<div class='product-image-wrapper'>";
 							echo "<div class='single-products'>";
 							echo "<div class='productinfo text-center'>";
